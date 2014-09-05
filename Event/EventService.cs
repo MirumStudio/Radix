65846e11d7718508ef864ad4e1601b30c7f13b58
vince14evil <vince14_evil@hotmail.com>
2014-09-05 09:49:05 -0400

    Add Service Management
    + Add file for service management
    + Add TypeUtility

--- Event/EventService.cs
+++ Event/EventService.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 
-
-
+namespace Radix.Event
+{
+    class EventService
+    {
+    }
+}
