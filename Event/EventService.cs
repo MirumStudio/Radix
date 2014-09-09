722e8bc8c07498febb518a603e7a46b9537b6b99
vince14evil <vince14_evil@hotmail.com>
2014-09-09 08:53:43 -0400

    Add Event management

--- Event/EventService.cs
+++ Event/EventService.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Radix.Service;
+using Radix.Utilities;
+using System;
 
 namespace Radix.Event
 {
-    class EventService
+    public delegate void EventReceiverHandler(Enum _event, object _args);
+    public class EventService : ServiceBase
     {
+        private static EventService instance = null;
+
+        protected override void Init()
+        {
+            instance = this;
+        }
+
+        protected override void Dispose()
+        {
+            instance = null;
+        }
+
+        private EventDispatcher mInternalEventDispatcher = new EventDispatcher();
+        private EventDispatcher mExternalEventDispatcher = new EventDispatcher();
+
+        static internal void RegisterEventListener(EventListener _listener)
+        {
+            if (IsInternalEvent(_listener.Event))
+            {
+                instance.mInternalEventDispatcher.RegisterEventListener(_listener);
+            }
+            else
+            {
+                instance.mExternalEventDispatcher.RegisterEventListener(_listener);
+            }
+        }
+
+        static internal void UnregisterAllEventListener(Type _listenerParent)
+        {
+            instance.mInternalEventDispatcher.UnregisterAllEventsListeners(_listenerParent);
+            instance.mExternalEventDispatcher.UnregisterAllEventsListeners(_listenerParent);
+        }
+
+        static public void DipatchEvent(Enum _event, object _args = null, Type _listernerType = null)
+        {
+            if (IsInternalEvent(_event))
+            {
+                instance.mInternalEventDispatcher.DispatchEvent(_event, _args, _listernerType);
+            }
+            else
+            {
+                instance.mExternalEventDispatcher.DispatchEvent(_event, _args, _listernerType);
+            }
+        }
+
+        static private bool IsInternalEvent(Enum _event)
+        {
+            return TypeUtility.IsInNamespace(_event.GetType(), "InternalEvent");
+        }
     }
 }
---
+++ Utlities/JsonUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Radix.Utilities
+{
+    public class JsonUtility
+    {
+        public static void SaveJsonToFile<T>(T aJsonObject, string aPath)
+        {
+            DeleteFileIfExist(aPath);
+
+            FileStream fileStream = File.Create(aPath);
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+
+            ser.WriteObject(fileStream, aJsonObject);
+
+            fileStream.Close();
+        }
+
+        public static T LoadJsonFromFile<T>(string aPath)
+        {
+            if (File.Exists(aPath))
+            {
+                FileStream fileStream = File.Open(aPath, FileMode.Open);
+
+                fileStream.Position = 0;
+
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+
+                return (T)ser.ReadObject(fileStream);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        private static void DeleteFileIfExist(string aPath)
+        {
+            if (File.Exists(aPath))
+            {
+                File.Delete(aPath);
+            }
+        }
+    }
+}
